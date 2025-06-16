7ecbe5db07c34998a289c2f1a202f2c40d481220
Nos <zvezdazanatih@gmail.com>
2025-06-16 12:35:10 +0700

    26

--- src/app/reg/reg.component.ts
+++ src/app/reg/reg.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-reg',
   templateUrl: './reg.component.html',
-  styleUrls: ['./reg.component.css']
+  styleUrls: ['./reg.component.css'],
+  
 })
-export class RegComponent {
+export class RegComponent implements OnInit {
 
   email: String;
   password: String;
 
+  constructor() {}
+  ngOnInit() {   
+  }
   userRegisterClick() {
     console.log(this.email);
     return false;
