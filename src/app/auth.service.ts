0c826f6e3409f12d25e8b7c83efdef53bc12f85e
Nos <zvezdazanatih@gmail.com>
2025-06-25 11:20:08 +0700

    51

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -3,11 +3,14 @@ import {User} from './interfaces'
 import {HttpClient} from '@angular/common/http'
 import { Observable } from 'rxjs';
 import { environment } from '../environments/environment.prod'; // подсказал ИИ
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+
+  private token =''
 
   constructor(private http: HttpClient) { }
  private baseUrl = environment.apiUrl; // подсказал ИИ
@@ -17,8 +20,31 @@ export class AuthService {
   login(user: User):Observable<{token: string}> {
     
     return this.http.post<{token: string}>(`${this.baseUrl}/api/auth/login`, user)
-    //return this.http.post<{token: string}>(`/api/auth/login`, user)
+    .pipe(
+      tap(
+        ({token}) => {
+          localStorage.setItem('auth-token', token)
+          this.setToken(token)
+        }
+      )
+    )
+    
   }
+setToken(token: string){
+  this.token = token
+}
 
+getToken(): string {
+  return this.token
+}
+
+isA(): boolean {
+  return !!this.token
+}
+
+logout(){
+  this.setToken('')
+  localStorage.clear()
+}
   
 }
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,23 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../auth.service';
+import { Subscription } from 'rxjs';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css']
 })
-export class AuthComponent implements OnInit{
+export class AuthComponent implements OnInit, OnDestroy {
 
   form1!: FormGroup;
+  aSub!: Subscription
 
-  constructor(private auth: AuthService) {}
+  constructor(private auth: AuthService, 
+              private router: Router,
+              private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     this.form1 = new FormGroup({
       email: new FormControl(null, [Validators.required, Validators.email]),
       password: new FormControl(null, [Validators.required, Validators.minLength(4)]),
     })
+    this.route.queryParams.subscribe((params: Params)=>{
+      if (params['registered']){
+        // можете
+      } else if (params['accessDenied']) {
+        // авторизуйтесь
+      }
+    })
+  }
+
+  ngOnDestroy(): void {
+    if(this.aSub){ this.aSub.unsubscribe() }    
+    
   }
     onSubmit(){
      if(this.form1.valid) {
@@ -28,9 +46,12 @@ export class AuthComponent implements OnInit{
     //   email: this.form1.value.email,
     //   password: this.form1.value.password
     //  }
-      this.auth.login(this.form1.value).subscribe(
-        () => console.log('Login success'),
-        error => {console.warn(error)}
+    this.form1.disable()
+      this.aSub = this.auth.login(this.form1.value).subscribe(
+        () => this.router.navigate(['/overview']),
+        error => {console.warn(error)
+        this.form1.enable()
+        }
       )
     }
 }
