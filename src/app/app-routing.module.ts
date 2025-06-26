6bd009f5636063fb08f587410aa4bfb30090f23d
Nos <zvezdazanatih@gmail.com>
2025-06-26 10:50:06 +0700

    57

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import {HomeComponent} from './home/home.component';
 import {RegComponent} from './reg/reg.component';
 import {AuthComponent} from './auth/auth.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
 {path: '', component: HomeComponent},
 {path: 'reg', component: RegComponent},
 {path: 'auth', component: AuthComponent},
-{path: 'dashboard', component: DashboardComponent},];
+{path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
