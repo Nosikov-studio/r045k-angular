a654d8ced1223f046056992a3b47dc68c1703d74
Nos <zvezdazanatih@gmail.com>
2025-06-27 10:32:11 +0700

    66

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -52,7 +52,8 @@ export class AuthComponent implements OnInit, OnDestroy {
         () => this.router.navigate(['/overview']),
         error => {
 const message = error.error?.message || error.message || 'Произошла ошибка при авторизации';
-  MaterialService.toast(message);
+//  MaterialService.toast(message);
+alert(message); // Заменили MaterialService.toast на alert
   console.warn(error);
   this.form1.enable();
         }
