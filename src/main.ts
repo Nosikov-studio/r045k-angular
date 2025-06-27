fbc9c76b1d22eedc6c9ac2f75d13a8bd6c0bcbe1
Nos <zvezdazanatih@gmail.com>
2025-06-27 12:47:26 +0700

    77

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
 //import 'materialize-css/dist/js/materialize.js';
-import 'materialize-css';
+//import 'materialize-css';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
