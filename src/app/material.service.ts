df0adbed32e76dd73b9aac94249641eb0908ac4d
Nos <zvezdazanatih@gmail.com>
2025-06-27 13:19:56 +0700

    79

--- src/app/material.service.ts
+++ src/app/material.service.ts
@@ -4,39 +4,39 @@ export class MaterialService {
     // static toast(message:string) {
     //     M.toast({html: message});
     // }
-    // static toast(message: string) {
-    //     console.log('MaterialService.toast вызван с сообщением:', message);
+    static toast(message: string) {
+        console.log('MaterialService.toast вызван с сообщением:', message);
         
-    //     // Проверяем, доступен ли Materialize
-    //     if (typeof M !== 'undefined' && M.toast) {
-    //         console.log('Materialize доступен, показываем toast');
-    //         const toast = M.toast;
-    //        // M.toast({html: message, displayLength: 4000});
-    //        // Создаем toast с правильными параметрами
-    //         toast({
-    //             html: message,
-    //             displayLength: 4000,
-    //             activationPercent: 0.8,
-    //             inDuration: 300,
-    //             outDuration: 375,
-    //             classes: 'red' // Добавляем красный цвет для ошибок
-    //         });
-    //     } else {
-    //         console.warn('Materialize недоступен, используем alert');
-    //         alert(message);
-    //     }
-    // }
-
-static toast(message: string) {
-    if (M && M.toast) {
-      M.toast({
-        html: message,
-        classes: 'red',         // класс для красного цвета (ошибка)
-        displayLength: 4000     // время показа в мс
-      });
-    } else {
-      alert(message);
+        // Проверяем, доступен ли Materialize
+        if (typeof M !== 'undefined' && M.toast) {
+            console.log('Materialize доступен, показываем toast');
+            const toast = M.toast;
+           // M.toast({html: message, displayLength: 4000});
+           // Создаем toast с правильными параметрами
+            toast({
+                html: message,
+                displayLength: 4000,
+                activationPercent: 0.8,
+                inDuration: 300,
+                outDuration: 375,
+                classes: 'red' // Добавляем красный цвет для ошибок
+            });
+        } else {
+            console.warn('Materialize недоступен, используем alert');
+            alert(message);
+        }
     }
+
+// static toast(message: string) {
+//     if (M && M.toast) {
+//       M.toast({
+//         html: message,
+//         classes: 'red',         // класс для красного цвета (ошибка)
+//         displayLength: 4000     // время показа в мс
+//       });
+//     } else {
+//       alert(message);
+//     }
   }
 
     
