522d5a63d06294e3ff60cbcf9ec7a33bea7a01e7
Nos <zvezdazanatih@gmail.com>
2025-06-30 09:22:17 +0700

    90

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategService } from '../categ.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  constructor(private categService: CategService){}
+
+  ngOnInit(){
+    this.categService.fetch().subscribe( cat=>{
+      console.log(cat)
+    })
+  }
 
 }
