fd1ade345a007ae16b6d9851d11b3577575aa063
Nos <zvezdazanatih@gmail.com>
2025-06-30 13:28:10 +0700

    99

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {CheckformService} from './checkform.service';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { TokenInterceptor } from './token.interceptor';
+import { LoaderComponent } from './loader/loader.component';
 
 // import { RouterModule, Routes } from '@angular/router';
 
@@ -31,7 +32,8 @@ import { TokenInterceptor } from './token.interceptor';
     AuthComponent,
     DashboardComponent,
     HomeComponent,
-    FooterComponent
+    FooterComponent,
+    LoaderComponent
   ],
   imports: [
     BrowserModule,
