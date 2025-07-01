9b33e5247617d88b909d8db9985903f9253edabd
Nos <zvezdazanatih@gmail.com>
2025-07-01 13:58:36 +0700

    108

--- src/app/dashboard/categories-form/categories-form.component.ts
+++ src/app/dashboard/categories-form/categories-form.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-categories-form',
   templateUrl: './categories-form.component.html',
   styleUrls: ['./categories-form.component.css']
 })
-export class CategoriesFormComponent {
+export class CategoriesFormComponent implements OnInit{
+  isNew =true
+  constructor(private route: ActivatedRoute){}
+
+  ngOnInit(): void {
+
+    this.route.params.subscribe((params: Params)=>{
+if(params['id']){
+  // Мы редактируем форму
+  this.isNew = false
+}
+    } )
+    
+  }
 
 }
