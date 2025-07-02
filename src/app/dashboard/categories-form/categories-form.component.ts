0c6825a3cf0e696b68cd95ebf1815f3f9a7fdc05
Nos <zvezdazanatih@gmail.com>
2025-07-02 13:38:06 +0700

    120

--- src/app/dashboard/categories-form/categories-form.component.ts
+++ src/app/dashboard/categories-form/categories-form.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 import { switchMap, of } from 'rxjs';
 import { CategService } from 'src/app/categ.service';
+import { Category } from 'src/app/interfaces';
 import { MaterialService } from 'src/app/material.service';
 
 @Component({
@@ -14,7 +15,7 @@ export class CategoriesFormComponent implements OnInit{
   @ViewChild('input') inputRef!: ElementRef
   form3!: FormGroup
   image!: File
-  imagePreview!: string | ArrayBuffer | null
+  imagePreview!: string | ArrayBuffer | null | undefined;
   isNew =true
   constructor(private route: ActivatedRoute,
               private categService: CategService
@@ -45,12 +46,12 @@ export class CategoriesFormComponent implements OnInit{
           }
         )
       ).subscribe(
-        category=> {
+        (category: Category|null)=> {
           if (category){
             this.form3.patchValue({
               name: category.name
-            }
-            )
+            })
+            this.imagePreview = category.imageSrc
             MaterialService.updateTextInputs()
           }
          // this.form3.enable()
