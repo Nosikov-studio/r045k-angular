e38107e5801da7b60436919afd7b532f9bc73eb0
Nos <zvezdazanatih@gmail.com>
2025-07-02 09:22:01 +0700

    110

--- src/app/dashboard/categories-form/categories-form.component.ts
+++ src/app/dashboard/categories-form/categories-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
@@ -7,10 +8,14 @@ import { ActivatedRoute, Params } from '@angular/router';
   styleUrls: ['./categories-form.component.css']
 })
 export class CategoriesFormComponent implements OnInit{
+  form3!: FormGroup
   isNew =true
   constructor(private route: ActivatedRoute){}
 
   ngOnInit(): void {
+    this.form3 = new FormGroup({
+      name: new FormControl(null, Validators.required)
+    })
 
     this.route.params.subscribe((params: Params)=>{
 if(params['id']){
@@ -18,6 +23,10 @@ if(params['id']){
   this.isNew = false
 }
     } )
+    
+  }
+
+  onSubmit(){
     
   }
 
